0e563e836d05de7ab71f7a6b61927345f7ded41e
josephchimebuka <chimebujoseph@gmail.com>
2024-04-18 10:32:05 +0000

    added the recent changes

---
+++ AdminDashboard/src/components/Navbar.tsx
@@ -0,0 +1,87 @@
+import { ReactElement, useEffect } from "react"
+import { AiOutlineMenu } from "react-icons/ai"
+import { FiShoppingCart } from "react-icons/fi"
+import { BsChatLeft } from "react-icons/bs"
+import { RiNotification3Fill } from "react-icons/ri"
+import { MdKeyboardArrowDown } from "react-icons/md"
+import { TooltipComponent } from "@syncfusion/ej2-react-popups"
+import { usestateContext } from "../context/ContextProvider"
+import avatar from '../data/avatar.jpg'
+
+interface NavButtonProps{
+  title: string
+  color: string,
+  dotColor: string,
+  icon: ReactElement
+  customFunc: ()=>void,
+}
+
+
+const NavButton =({title, icon, customFunc, color, dotColor}:NavButtonProps)=>(
+  <TooltipComponent content={title} position="BottomCenter">
+    <button type="button" style={{color}} className="relative text-xl rounded-full hover:bg-light-gray" onClick={customFunc}>
+      <span style={{background: dotColor}} className="absolute inline-flex rounded-full h-2 w-2 -right-1 top-0"/>
+      {icon}
+   
+    </button>
+  </TooltipComponent>
+)
+const Navbar = () => {
+  const {setActiveMenu} = usestateContext()
+
+  const handleToggleMenu = () => {
+    setActiveMenu(); 
+  };
+  return (
+    <div className="flex items-center justify-between md:mx-6">
+      <NavButton
+       title={"Menu"}
+        color={"blue"}
+         dotColor={""}
+          icon={<AiOutlineMenu/>}
+           customFunc={handleToggleMenu}
+           />
+      <div className="flex items-center gap-5">
+      <NavButton
+       title={"Cart"}
+        color={"blue"}
+         dotColor={""}
+          icon={<FiShoppingCart/>}
+           customFunc={handleToggleMenu}
+           />
+
+    <NavButton
+       title={"Cart"}
+        color={"blue"}
+         dotColor={"#03C9D7"}
+          icon={<BsChatLeft/>}
+           customFunc={handleToggleMenu}
+           />
+
+
+    <NavButton
+       title={"Cart"}
+        color={"blue"}
+          dotColor={"#03C9D7"}
+          icon={<RiNotification3Fill/>}
+           customFunc={handleToggleMenu}
+           />
+
+
+          <TooltipComponent content='Profile' position="BottomCenter">
+          <div className="flex items-center cursor-pointer p-3 hover:bg:light-gray rounded-lg" onClick={handleToggleMenu}>
+              <img src={avatar} className="rounded-full w-8 h-8"/>
+              <p>
+                <span className="text-gray-400 text-14">Hi,</span> {'  '}
+                <span className="text-gray-400 ml-1 text-14">Joseph</span>
+              </p>
+              <MdKeyboardArrowDown className="text-gray-400 ml-1 text-14"/>
+           </div>
+          </TooltipComponent>
+         
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ AdminDashboard/src/components/Sidebar.tsx
@@ -0,0 +1,52 @@
+import { Link, NavLink } from "react-router-dom"
+import {SiShopware} from 'react-icons/si'
+import {MdOutlineCancel} from 'react-icons/md'
+import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import {links} from '../data/dummy'
+import { usestateContext } from "../context/ContextProvider"
+const Sidebar = () => {
+  const {activeMenu,setActiveMenu} =  usestateContext()
+  const handleToggleMenu = () => {
+    setActiveMenu(); 
+  };
+  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
+  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray  text-md m-2'
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto
+     md:hover:overflow-auto pb-10">
+        {
+          activeMenu &&(
+            <><div className="flex justify-between items-center">
+                <Link to='/' className="flex items-center gap-3 ml-3 mt-4 text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+                  <SiShopware/> <span>ShopStores</span>
+                </Link>
+                <TooltipComponent content='menu'>
+                    <button onClick={handleToggleMenu}
+                     className="p-3 text-xl rounded-full hover:bg-light-gray md:hidden">
+                      <MdOutlineCancel/>
+                    </button>
+                </TooltipComponent>
+            </div>
+
+            <div>
+            {links.map((item)=>(
+              <div  key={item.title}>
+            <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
+            {item.links.map((link)=>(
+              <NavLink 
+              to={`/${link.name}`}
+              className={({isActive}) => isActive ? activeLink :  normalLink}
+              >{link.icon}
+              <span className="capitalize">{link.name}</span></NavLink>
+            ))}
+              </div>
+            ))}
+            </div>
+            </>
+          )
+        }
+      </div>
+  )
+}
+
+export default Sidebar
